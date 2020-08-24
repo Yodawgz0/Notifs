2eff5e4cea39b76088994e35deaa6716064be01a
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2020-08-25 00:02:47 +0530

    added CSS to modifynotifs

--- src/components/ModifyNotifications/ModifyNotifs.js
+++ src/components/ModifyNotifications/ModifyNotifs.js
@@ -4,10 +4,14 @@ import firebase from "../../firebase.js";
 import Button from "@material-ui/core/Button";
 import DeleteIcon from "@material-ui/icons/Delete";
 import Divider from "@material-ui/core/Divider";
+import Link from "@material-ui/core/Link";
+import { storage } from "../../firebase.js";
+import { useStyles } from "./ModifyNotifsStyle.js";
 
 function useNotifs() {
+  
+  
   const [Notifcations, setNotifcations] = useState([]);
-
 
   useEffect(() => {
     firebase
@@ -27,20 +31,24 @@ function useNotifs() {
 
 const ModifyNotifs = () => {
   const notifs = useNotifs();
+  const classes = useStyles();
   return (
     <div>
-      <h2>List of Notifications</h2>
+      <h2 className={classes.heading}>List of Notifications</h2>
+      <Divider />
       <div>
         {notifs.map((notif) => (
           <div>
             <Row key={notif.id} style={{ padding: "0.5rem" }}>
-              <Col className="title">{notif.title}</Col>
-              <Col className="content">{notif.content}</Col>
-              <Col className="date"> {notif.date}</Col>
-              <Col className="priority">{notif.value}</Col>
-              <Col className="link">{notif.url}</Col>
-              <Col>
-                <div className="Cross">
+              <Col sm={12} className="title">{notif.title}</Col>
+              <Col sm={12}  className="content">{notif.content}</Col>
+              <Col xs={5} sm={5} md={3} lg={3} xl={3} className="date"> {notif.date}</Col>
+              <Col xs={2} sm={2} md={3} lg={3} xl={3} className="priority">{notif.value}</Col>
+              <Col xs={12} sm={12} md={3} lg={3} xl={3} className="Link" >
+                <Link href={notif.url}  onClick={(notif.url)? "":(e)=> e.preventDefault()}>View File</Link>
+              </Col>
+              <Col xs={1} sm={2} md={2} lg={2} xl={2}>
+                <div className="Delete">
                   <Button
                     variant="contained"
                     color="secondary"
@@ -53,6 +61,8 @@ const ModifyNotifs = () => {
                         .collection("Notifcations")
                         .doc(notif.id)
                         .delete();
+                      storage.refFromURL(notif.url).delete();
+                      
                     }}
                   >
                     Delete
@@ -60,7 +70,7 @@ const ModifyNotifs = () => {
                 </div>
               </Col>
             </Row>
-            <Divider variant="inset" />
+            <Divider variant="middle" />
           </div>
         ))}
       </div>
---
+++ src/components/ModifyNotifications/ModifyNotifsStyle.js
@@ -0,0 +1,7 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  heading: { textAlign: "center", marginBottom: "5%" },
+}));
+
+export { useStyles };
